7774d7d0b8e199c9486596283f70cd54c7f6c497
sam-bee <130986804+sam-bee@users.noreply.github.com>
2024-09-02 00:50:59 +0100

    To allow development of crossovers during breeding, add failing test

--- pkg/geneticalgorithm/ga_test.go
+++ pkg/geneticalgorithm/ga_test.go
@@ -1,8 +1,9 @@
 package geneticalgorithm
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // We define a fake randomiser for testing purposes
@@ -24,7 +25,7 @@ func (r *fakeRandomiser) prepop(rands []float64) {
 	r.vals = rands
 }
 
-func testGenotypeEqualityCheck(t *testing.T) {
+func TestGenotypeEqualityCheck(t *testing.T) {
 	// We test the equality check for genotypes
 	gene1 := gene{tailNode: 1, headNode: 2}
 	gene2 := gene{tailNode: 1, headNode: 4}
@@ -38,7 +39,7 @@ func testGenotypeEqualityCheck(t *testing.T) {
 	assert.False(t, genotype1.equals(genotype3))
 }
 
-func testBreedWithoutCrossoverOrMutation(t *testing.T) {
+func TestBreedWithoutCrossoverOrMutation(t *testing.T) {
 
 	// We test breeding with no crossover or mutation
 	crossoverRate := 0.0
@@ -59,5 +60,28 @@ func testBreedWithoutCrossoverOrMutation(t *testing.T) {
 
 	assert.True(t, firstChildIsCloneOfParent)
 	assert.True(t, secondChildIsCloneOfParent)
+}
 
+func TestBreedWithCrossover(t *testing.T) {
+
+	t.Skip("Not implemented yet")
+
+	// We test breeding with crossover
+	crossoverRate := 1.0
+	mutationRate := 0.0
+
+	org1 := organism{genotype: []gene{{1, 2}, {3, 4}}}
+	org2 := organism{genotype: []gene{{5, 6}, {7, 8}}}
+	parents := [2]organism{org1, org2}
+
+	// The crossover rate will always be higher than the random number generated, so crossover will occur
+	rand := fakeRandomiser{vals: []float64{0.5}}
+
+	children := breed(parents, &rand, crossoverRate, mutationRate)
+
+	firstChildIsCorrect := children[0].genotype.equals([]gene{{1, 2}, {7, 8}})
+	secondChildIsCorrect := children[1].genotype.equals([]gene{{5, 6}, {3, 4}})
+
+	assert.True(t, firstChildIsCorrect)
+	assert.True(t, secondChildIsCorrect)
 }
