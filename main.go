0fd8c99569142eeb6d525ce665e48be295a5d494
sam-bee <130986804+sam-bee@users.noreply.github.com>
2024-09-12 20:42:20 +0100

    To provide basic default values for key variables of the genetic algorithm, put a few values in main.go for now

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var GenotypeLength = 1024
+var PopulationSize = 100
+var MutationRate = 0.01
+var CrossoverRate = 0.7
+
 func main() {
 	fmt.Println("Hello, World!")
 }
